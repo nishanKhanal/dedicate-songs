30da0c4e40f1f8658a194ffa8e009ca6f97db02e
Nishan Khanal <nishan.khanal98@gmail.com>
2020-04-12 19:46:27 +0545

    Added posts as stacked in lines for users

--- users/admin.py
+++ users/admin.py
@@ -3,13 +3,22 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from posts.models import Post
 # Register your models here.
+
+class PostInline(admin.StackedInline):
+    model = Post
+    extra = 1
+
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username', 'age', 'is_staff']
+    inlines = [
+        PostInline,
+    ]
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
