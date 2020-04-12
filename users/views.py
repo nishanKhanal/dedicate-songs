2ff8ef3748c9322222263aec6cedf30a344b1a83
Nishan Khanal <nishan.khanal98@gmail.com>
2020-04-12 11:14:15 +0545

    Added signup and logout feature

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
+from .models import CustomUser
+from .forms import CustomUserCreationForm
 # Create your views here.
+
+class SignUpView(CreateView):
+    success_url = reverse_lazy('login')
+    form_class = CustomUserCreationForm
+    template_name = "users/signup.html"
+
+
+    def form_valid(self, form):
+        self.object = form.save()
+        full_name = self.request.POST['full_name']
+        last_name = full_name.split(' ')[-1]
+        first_name = full_name.replace(last_name,'')[:-1]
+        self.object.first_name = first_name
+        self.object.last_name = last_name
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
