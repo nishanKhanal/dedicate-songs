756663fbebe4ed5a9ce36db4a31aa7704dace896
Nishan Khanal <nishan.khanal98@gmail.com>
2020-04-12 14:49:03 +0545

    Set up two different post create view for registered and unregistered users

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
 class Post(models.Model):
-    post_from = models.CharField(max_length=50)
+    post_from = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    post_from_public = models.CharField(max_length=50,blank=True,null=True,default="public user")
     post_to = models.CharField(max_length=50)
     message = models.TextField(default=" ")
     video_link = models.CharField(max_length=200, default="https://youtube.com")
@@ -14,7 +16,11 @@ class Post(models.Model):
     video_title = models.CharField(max_length=250, default='Title of this song')
 
     def __str__(self):
-        return f"from: {self.post_from}, to: {self.post_to}, message:{self.message}"
+        if self.post_from.username == "publicuser":
+            post_from_name = self.post_from_public
+        else:
+            post_from_name = self.post_from.username 
+        return f"from: {post_from_name}, to: {self.post_to}, message:{self.message}"
 
     def get_absolute_url(self):
         return reverse("posts:post_detail", kwargs={"pk": self.pk})
